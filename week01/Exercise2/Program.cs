e8d797e544f1e0464f1b921a62459b6b8e86028b
jasperyaona <jyaona@byupathway.edu>
2026-09-03 22:39:34 +0800

    Changes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        int percent = int.Parse(Console.ReadLine());
+
+        string grade = "";
+        string sign = "";
+
+        if(percent>=90)
+        {
+             grade = "A";
+        
+        }
+        else if(percent>=80)
+        {
+            grade = "B";
+        }
+        else if(percent>=70)
+        {
+            grade = "C";
+        }
+        else if(percent>=60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        int lastDigit = percent%10;
+
+        if(lastDigit>=7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+
+
+         if (grade == "A")
+        {
+            sign = lastDigit < 3 ? "-" : "";
+        }
+         if (grade == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {grade}{sign}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+
     }
 }
